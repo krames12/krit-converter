5359b24555c24577309ccd198c3cc7786dda34b8
Nick Kramer <krames12@gmail.com>
2024-05-18 18:47:30 -0400

    adds header with static file path

--- main.go
+++ main.go
@@ -287,6 +287,7 @@ func main() {
 	http.HandleFunc("/", uploadHandler)
 	http.HandleFunc("/result/", resultHandler)
 	http.Handle("/uploads/", http.StripPrefix("/uploads/", http.FileServer(http.Dir("./uploads"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	log.Println("Starting server on :8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
